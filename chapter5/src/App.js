01287ad721809add4a827ba37185d6721c3119f2
DaniParkHoJin <parkhojin@naver.com>
2024-07-11 18:20:55 +0900

    7/11 react-project1 the end

--- chapter5/src/App.js
+++ chapter5/src/App.js
@@ -3,26 +3,20 @@ import Header from "./component/Header";
 import Body from "./component/Body";
 import Footer from "./component/Footer";
 
-// function Header() {
-//   return (<header>
-//     <h1>header</h1>
-//   </header>
-//   );
-// }
 
-// const Header = () => {
-//   return(
-//       <header>
-//         <h1>header</h1>
-//       </header>
-//   );
-// };
+const ChildComp = () => {
+    return <div>child component</div>;
+}
 
 function App() {
+    //const BodyProps = {name: "박호진", location: "서울시", //favorList: ["파스타", "빵", "떡볶이"],
+   // }
     return (
         <div className="App">
             <Header/>
-            <Body/>
+            <Body>
+                <ChildComp />
+            </Body>
             <Footer/>
         </div>
     );
