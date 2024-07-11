98d6b80c8226ad188930b586052609f38366e0e4
DaniParkHoJin <parkhojin@naver.com>
2024-07-12 06:13:19 +0900

    lotto

---
+++ lotto/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Viewer from "./component/Viewer";
+import {useState} from "react";
+
+function App() {
+  return (
+      const [number, setNumber] = useState(0);
+      const lottoNumber = () => {
+
+      }
+
+    <div className="App">
+      <Viewer></Viewer>
+    </div>
+  );
+}
+
+export default App;
---
+++ lotto/src/component/controller.js
@@ -0,0 +1,11 @@
+const controller = () => {
+
+    return (
+            <div>
+                <button onClick={ () => }>1번째 숫자</button>
+
+            </div>
+
+    );
+}
+export default controller
